294430912e76eecd8a49b57eaff9c2ecaa9f63f7
mayalh22 <maya.l.hazarika@gmail.com>
2025-12-06 14:34:30 -0500

    added header.html

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,4 +1,3 @@
-
 async function loadJSON(path) {
   const response = await fetch(path);
   return await response.json();
@@ -7,4 +6,21 @@ async function loadJSON(path) {
 async function insertJSON(path, elementId, template) {
   const data = await loadJSON(path);
   document.getElementById(elementId).innerHTML = template(data);
+}
+
+async function loadHeader(isSubpage = false) {
+  const headerPath = isSubpage ? '../header.html' : 'header.html';
+  const response = await fetch(headerPath);
+  const html = await response.text();
+  
+  // Adjust paths if on subpage
+  let adjustedHtml = html;
+  if (isSubpage) {
+    adjustedHtml = html
+      .replace(/href="index\.html"/g, 'href="../index.html"')
+      .replace(/href="subpages\//g, 'href="')
+      .replace(/src="assets\//g, 'src="../assets/');
+  }
+  
+  document.getElementById('header-container').innerHTML = adjustedHtml;
 }
