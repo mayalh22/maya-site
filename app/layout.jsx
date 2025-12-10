adbd6feaf8a227037005ce432986697b6a399798
mayalh22 <maya.l.hazarika@gmail.com>
2025-12-09 23:07:27 -0500

    fixed photo size, css, profile etc...

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,14 +4,18 @@ export const metadata = {
 };
 
 import '../styles/colors.css';
-import '../styles/components.css';
 import '../styles/layout.css';
-
+import '../styles/components.css';
 import Header from '@/components/Header';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <Header />
         <main>{children}</main>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,62 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="header">
-      <nav className="navbar">
-        <h1 className="logo">Maya Hazarika</h1>
-        <ul className="nav-links">
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/art">Art</Link></li>
-          <li><Link href="/code-projects">Code Projects</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
-          <li><Link href="/favorites">Favorites</Link></li>
-          <li><Link href="/photos">Photos</Link></li>
-          <li><Link href="/timeline">Timeline</Link></li>
-          <li><Link href="/writing">Writing</Link></li>
+    <header className="site-header">
+      <Image 
+        src="/assets/name-title.png" 
+        alt="Maya Hazarika" 
+        width={280} 
+        height={80}
+        className="name-title"
+        priority
+      />
+      
+      <nav className="image-nav">
+        <ul>
+          <li>
+            <Link href="/art">
+              <Image src="/assets/art-button.png" alt="Art" width={60} height={60} />
+              <span>Art</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/code-projects">
+              <Image src="/assets/code-projects-button.png" alt="Code Projects" width={60} height={60} />
+              <span>Code</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <Image src="/assets/contact-button.png" alt="Contact" width={60} height={60} />
+              <span>Contact</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/favorites">
+              <Image src="/assets/favorites-button.png" alt="Favorites" width={60} height={60} />
+              <span>Favorites</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/photos">
+              <Image src="/assets/photos-button.png" alt="Photos" width={60} height={60} />
+              <span>Photos</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/timeline">
+              <Image src="/assets/timeline-button.png" alt="Timeline" width={60} height={60} />
+              <span>Timeline</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/writing">
+              <Image src="/assets/writing-button.png" alt="Writing" width={60} height={60} />
+              <span>Writing</span>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
