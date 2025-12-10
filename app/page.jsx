b413e1d3d22abec1de175fb1206db6bdfeae4a4a
mayalh22 <maya.l.hazarika@gmail.com>
2025-12-09 23:09:07 -0500

    fixed profile pic dimensions

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ import aboutData from '@/lib/content/about.json';
 export default function Home() {
   return (
     <section id="about-section" className="about">
-      <img src={`/assets/${aboutData.photo}`} alt="Maya" className="profile" />
+      <img src={`/assets/${aboutData.photo}`} width={280} height={200} alt="Maya" className="profile" />
       <h1>{aboutData.name}</h1>
       <h3>{aboutData.tagline}</h3>
       <p>{aboutData.bio}</p>
