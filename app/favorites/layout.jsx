96913ee9a6b5504fd7d7c3e470154b228109a199
mayalh22 <maya.l.hazarika@gmail.com>
2026-02-23 10:57:04 -0500

    added layout page for favorites

---
+++ app/favorites/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Favorites',
+  description: 'My favorite books, music, and movies.',
+};
+
+export default function FavoritesLayout({ children }) {
+  return <>{children}</>;
+}
