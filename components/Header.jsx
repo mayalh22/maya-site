8cd42dfaee280910b07f0c4a13f4780558e129bd
mayalh22 <maya.l.hazarika@gmail.com>
2026-02-23 09:32:13 -0500

    remove duplicate Link import

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
-import Link from 'next/link';
 import Image from 'next/image';
-
+ 
 export default function Header() {
   return (
     <header className="site-header">
