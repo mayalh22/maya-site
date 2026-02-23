b7182bc33deb5b5366dc6b534fd7a671aa24ab10
mayalh22 <maya.l.hazarika@gmail.com>
2026-02-23 12:06:36 -0500

    moved metadata from page code projects to laout code projects

---
+++ app/code-projects/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Code Projects',
+  description: "My coding projects and portfolio.",
+};
+
+export default function CodeProjectsLayout({ children }) {
+  return <>{children}</>;
+};
