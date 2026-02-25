00f3845ab1702bafda8809f1cf3150793bcdf323
mayalh22 <maya.l.hazarika@gmail.com>
2026-02-24 22:19:32 -0500

    no more metadata for app/page :(

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,6 @@
 import Image from 'next/image';
 import aboutData from '@/lib/content/about.json';
 
-export const metadata = {
-  title: 'Maya Hazarika',
-  description: "My personal site.",
-};
-
 export default function Home() {
   return (
     <section id="about-section" className="about striped-section">
